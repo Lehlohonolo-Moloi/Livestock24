e73cb5acb329ed99159d7b9f58bd6d310a0308ce
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2023-03-17 14:52:55 +0200

    Added the source code

---
+++ livestock48/src/main/java/com/Goats/livestock48/Livestock48Application.java
@@ -0,0 +1,13 @@
+package com.Goats.livestock48;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Livestock48Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Livestock48Application.class, args);
+	}
+
+}
---
+++ livestock48/src/test/java/com/Goats/livestock48/Livestock48ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Goats.livestock48;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Livestock48ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
