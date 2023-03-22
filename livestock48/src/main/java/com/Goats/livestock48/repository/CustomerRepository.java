80eb3a4eaf9898074454d8cfa511b8175051b68a
Thabang Kubheka <thabang.kubheka@vcontractor.co.za>
2023-03-22 14:52:09 +0200

    Added some changes

--- livestock48/src/main/java/com/Goats/livestock48/repository/CustomerRepository.java
+++ livestock48/src/main/java/com/Goats/livestock48/repository/CustomerRepository.java
@@ -1,15 +1,18 @@
 package com.Goats.livestock48.repository;
 
+import com.Goats.livestock48.model.Animal;
 import com.Goats.livestock48.model.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
-    @Query("SELECT c FROM Customer c WHERE c.email = ?1")
-    Optional<Customer> findCustomerByEmail(String email);
+    @Query(value="select * from Customer e where e.first_name like %:keyword%  or e.last_name like %:keyword%", nativeQuery=true)
+    List<Customer> findByKeyword(@Param("keyword") String keyword);
 }
